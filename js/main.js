d4a382e6acd23c1a59a8bd0fe9715d47498f5c5d
danny <danny@xmpie.com>
2017-01-15 22:11:41 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,24 @@
+(function ($) {
+    $(document).ready(function(){		
+           
+        $.get("https://api.github.com/users/basdanny/gists", function(data) {            
+            jPut.gist.data = data;
+            $(".gist .fa-spin").hide();
+        });
+                                          
+        $(".stackoverflow .fa-spin").hide();
+
+        $(".somethingtoread").click(function() {           
+            $(".stackoverflow").show();            
+        }); 
+
+        $("li.filter").click(function() {
+           jPut.stackoverflow.data = [];
+            $(".stackoverflow .fa-spin").show();
+            $.get("https://api.stackexchange.com//2.2/search?order=desc&sort=votes&tagged="+encodeURIComponent($(this).text())+"&site=stackoverflow", function(data) {            
+                jPut.stackoverflow.data = data.items;
+                $(".stackoverflow .fa-spin").hide();
+            });
+        }); 
+    });
+}(jQuery));
