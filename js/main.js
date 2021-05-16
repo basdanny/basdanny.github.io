9d0f4df2bef05e18102ca11779e731652f8e0ce0
basdanny <basdanny@gmail.com>
2021-05-16 17:09:52 +0300

    styling

--- js/main.js
+++ js/main.js
@@ -1,24 +1,24 @@
 (function ($) {
-    $(document).ready(function(){		
-           
-        $.get("https://api.github.com/users/basdanny/gists", function(data) {            
+    $(document).ready(function () {
+
+        $.get("https://api.github.com/users/basdanny/gists", function (data) {
             jPut.gist.data = data;
             $(".gist .fa-spin").hide();
         });
-                                          
+
         $(".stackoverflow .fa-spin").hide();
 
-        $(".somethingtoread").click(function() {           
-            $(".stackoverflow").show();            
-        }); 
+        $(".somequestions").click(function () {
+            $(".stackoverflow").toggle();
+        });
 
-        $("li.filter").click(function() {
-           jPut.stackoverflow.data = [];
+        $("li.filter").click(function () {
+            jPut.stackoverflow.data = [];
             $(".stackoverflow .fa-spin").show();
-            $.get("https://api.stackexchange.com/2.2/search?order=desc&sort=votes&tagged="+encodeURIComponent($(this).text())+"&site=stackoverflow", function(data) {            
+            $.get("https://api.stackexchange.com/2.2/search?order=desc&sort=votes&tagged=" + encodeURIComponent($(this).text()) + "&site=stackoverflow", function (data) {
                 jPut.stackoverflow.data = data.items;
                 $(".stackoverflow .fa-spin").hide();
             });
-        }); 
+        });
     });
 }(jQuery));
